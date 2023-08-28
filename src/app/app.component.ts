e90418929d022cac4c267f17f9cbd49f86bd05fd
Philippe CORTEZ <philippe.cortez.pro@gmail.com>
2023-08-28 19:26:24 +0200

    Set prettier style in project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'genshin-community-front';
+    title = 'genshin-community-front';
 }
